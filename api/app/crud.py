e368e411677fe94b9b51808873638b94f5e01030
matyjb <matyjb14@gmail.com>
2024-06-20 20:06:12 +0200

    two api endpoints

--- api/app/crud.py
+++ api/app/crud.py
@@ -4,12 +4,12 @@ from sqlalchemy.orm import Session
 from . import models
 
 
-def generate_short_url(length=6):
+def generate_short_url(length: int = 6):
     characters = string.ascii_letters + string.digits
     return "".join(random.choice(characters) for _ in range(length))
 
 
-def shorten_url(db: Session, original_url):
+def shorten_url(db: Session, original_url: str):
     short_url_id = generate_short_url()
     while (
         db.query(models.URL).filter(models.URL.short_url_id == short_url_id).first()
@@ -24,6 +24,7 @@ def shorten_url(db: Session, original_url):
     db.commit()
     db.refresh(db_url)
     return db_url
+
 
 def get_url(db: Session, short_url_id: str):
     return db.query(models.URL).filter(models.URL.short_url_id == short_url_id).first()
--- api/app/main.py
+++ api/app/main.py
@@ -1,15 +1,33 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 
-from app.schemas import URLCreate
+from app import models
+from app import schemas
+from app.database import SessionLocal, engine
+from app import crud
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.post("/create")
-async def root(original_url: URLCreate):
-    return {"message": "Create url", "url": original_url.original_url}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/{id}")
-async def get_url(id: str):
-    return {"message": "Get url", "url": id}
+@app.post("/create", response_model=schemas.URL)
+async def shorten_url(original_url: schemas.URLCreate, db: Session = Depends(get_db)):
+    db_url = crud.shorten_url(db, original_url.original_url)
+    return db_url
+
+
+@app.get("/{id}", response_model=schemas.URL)
+async def get_url(id: str, db: Session = Depends(get_db)):
+    db_url = crud.get_url(db, id)
+    if db_url is None:
+        raise HTTPException(404, "Url not found")
+    return db_url
