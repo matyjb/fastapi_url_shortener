b8543518da50c91db5a357266357f5af99db5030
matyjb <matyjb14@gmail.com>
2024-06-20 18:42:08 +0200

    paths

--- api/app/main.py
+++ api/app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from app.schemas import URLCreate
+
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/create")
+async def root(original_url: URLCreate):
+    return {"message": "Create url", "url": original_url.original_url}
+
+
+@app.get("/{id}")
+async def get_url(id: str):
+    return {"message": "Get url", "url": id}
