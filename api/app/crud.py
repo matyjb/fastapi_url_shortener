814882e5474862ba7e026b1c90e7c7fe19b38cd0
matyjb <matyjb14@gmail.com>
2024-06-20 19:40:02 +0200

    crud functions

---
+++ api/app/crud.py
@@ -0,0 +1,29 @@
+import random
+import string
+from sqlalchemy.orm import Session
+from . import models
+
+
+def generate_short_url(length=6):
+    characters = string.ascii_letters + string.digits
+    return "".join(random.choice(characters) for _ in range(length))
+
+
+def shorten_url(db: Session, original_url):
+    short_url_id = generate_short_url()
+    while (
+        db.query(models.URL).filter(models.URL.short_url_id == short_url_id).first()
+        != None
+    ):
+        # try again
+        short_url_id = generate_short_url()
+
+    db_url = models.URL(original_url=original_url, short_url_id=short_url_id)
+
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    return db_url
+
+def get_url(db: Session, short_url_id: str):
+    return db.query(models.URL).filter(models.URL.short_url_id == short_url_id).first()
