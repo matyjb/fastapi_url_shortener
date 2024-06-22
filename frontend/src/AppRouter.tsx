5ca8ef8308d7ddb4214d2a24cdd97a85569c00bf
matyjb <matyjb14@gmail.com>
2024-06-22 15:55:48 +0200

    redirect page

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: ":linkId",
+        path: ":shortLinkId",
         element: <RedirectToUrl />,
       },
     ],
--- frontend/src/pages/redirect_to_url/RedirectToUrl.tsx
+++ frontend/src/pages/redirect_to_url/RedirectToUrl.tsx
@@ -1,18 +1,36 @@
-import { FunctionComponent, useEffect } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getUrl } from "../../api.service";
 
 interface RedirectToUrlProps {}
 
 const RedirectToUrl: FunctionComponent<RedirectToUrlProps> = () => {
   const params = useParams();
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
-    console.log(params);
-  });
+    if (!params.shortLinkId) return;
+
+    getUrl(params.shortLinkId).then((url) => {
+      if (url && url.original_url) {
+        window.location.replace(url.original_url);
+      } else {
+        setError("Url does not exist");
+      }
+    });
+  }, [params.shortLinkId]);
   // make request to api and redirect to provided original url
   // if it doesnt exist, display 404 error
 
-  return <>{params.linkId}</>;
+  return (
+    <div className="text-center text-2xl m-6">
+      {error ? (
+        <div className="text-red-500">{error}</div>
+      ) : (
+        <div className="text-indigo-400">Redirecting...</div>
+      )}
+    </div>
+  );
 };
 
 export default RedirectToUrl;
